10c965694f2a831cb674441c9f725cf663e2890a
jseg787 <jsegarra3@gmail.com>
2021-06-11 10:50:42 -0400

    move Seach styles to own file

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link, NavLink } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink,
+} from 'react-router-dom';
 import GetPlaylist from './pages/GetPlaylist';
 import LoginToSpotify from './pages/LoginToSpotify';
 import NavBar from './components/NavBar';
@@ -8,32 +14,37 @@ import { getHashParams, setAccessToken } from './SpotifyHelper';
 import './App.css';
 
 class App extends Component {
-	constructor() {
-		super();
-		const params = getHashParams();
-		this.state = {
-			access_token: params.access_token
-		};
-		if (params.access_token) {
-			setAccessToken(params.access_token);
-		}
-	}
+  constructor() {
+    super();
+    const params = getHashParams();
+    this.state = {
+      access_token: params.access_token,
+    };
+    if (params.access_token) {
+      setAccessToken(params.access_token);
+    }
+  }
 
-	render() {
-		return (
-			<Router>
-				<NavBar />
-				<div className="container-fluid text-center">
-					<div className="row justify-content-center">
-						<Switch>
-							<Route path="/login" component={LoginToSpotify} />
-							<Route path="/" render={() => <GetPlaylist loggedIn={this.state.access_token} />} />
-						</Switch>
-					</div>
-				</div>
-			</Router>
-		);
-	}
+  render() {
+    return (
+      <Router>
+        <NavBar />
+        <div className="container-fluid text-center">
+          <div className="row justify-content-center">
+            <Switch>
+              <Route path="/login" component={LoginToSpotify} />
+              <Route
+                path="/"
+                render={() => (
+                  <GetPlaylist loggedIn={this.state.access_token} />
+                )}
+              />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,35 +1,36 @@
 import React from 'react';
+import '../Search.css';
 
 function Search(props) {
-	const { numberOfSongs, updateNumber, incrementNumber, decrementNumber, getAPlaylist } = props;
+  const {
+    numberOfSongs,
+    updateNumber,
+    incrementNumber,
+    decrementNumber,
+    getAPlaylist,
+  } = props;
 
-	return (
-		<div className="d-flex justify-content-center mb-3">
-			<input
-				className="number"
-				type="number"
-				min="1"
-				max="20"
-				step="1"
-				onChange={updateNumber}
-				value={numberOfSongs}
-			/>
-			<div className="number-buttons d-flex flex-column">
-				<button className="change-number" onClick={incrementNumber}>
-					+
-				</button>
-				<button className="change-number" onClick={decrementNumber}>
-					-
-				</button>
-			</div>
-			<p className="text-light" style={{ fontSize: '1.5rem', margin: '.5rem 1rem' }}>
-				songs
-			</p>
-			<button className="btn btn-primary" onClick={getAPlaylist}>
-				Get a playlist
-			</button>
-		</div>
-	);
+  return (
+    <div className="Search">
+      <input
+        className="Search-number"
+        type="number"
+        min="1"
+        max="20"
+        step="1"
+        onChange={updateNumber}
+        value={numberOfSongs}
+      />
+      <div className="Search-number--buttons">
+        <button onClick={incrementNumber}>+</button>
+        <button onClick={decrementNumber}>-</button>
+      </div>
+      <p className="">songs</p>
+      <button className="" onClick={getAPlaylist}>
+        Get a playlist
+      </button>
+    </div>
+  );
 }
 
 export default Search;

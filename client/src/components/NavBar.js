fef2d5f835c73d938d7bf370f2a9e85cb6a58b7f
jseg787 <jsegarra3@gmail.com>
2021-06-11 12:46:43 -0400

    restyle navbar without bootstrap

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from '../logo.png';
+import '../styles/Navbar.css';
 
 function NavBar() {
-	return (
-		<nav className="navbar navbar-expand-sm navbar-dark bg-dark shadow mb-3">
-			<img src={logo} alt="logo" style={{ width: '1.8rem' }} />
-		</nav>
-	);
+  return (
+    <nav className="Navbar">
+      <img class="Navbar-logo" src={logo} alt="logo" />
+    </nav>
+  );
 }
 
 export default NavBar;
